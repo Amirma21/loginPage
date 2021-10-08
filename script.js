7c20b7394b5a6705df924a3bc0f7b4b59740da6b
amir maleki <amirmaleki21physics@gmail.com>
2021-10-08 14:31:41 +0330

    show and hide feature was added to form

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const signup = document.querySelector(".signup")
 const dynamicBtn = document.querySelector(".dynamic-btn")
 const signUpForm = document.querySelector(".signup-form")
 const loginForm = document.querySelector(".loginform")
+const passwordIcon = document.querySelector(".show-icon")
+const password = document.querySelector(".show-icon-parent")
+
 
 signup.addEventListener("click", () => {
     dynamicBtn.style.right = "50%"
@@ -16,5 +19,17 @@ login.addEventListener("click", () => {
     dynamicBtn.style.right = "0%"
     loginForm.style.display = "block"
     signUpForm.style.display = "none"
+
+})
+
+passwordIcon.addEventListener("click", () => {
+    if (password.type === "password") {
+        password.setAttribute("type", "text")
+        passwordIcon.classList.remove("hide")
+    } else {
+        password.setAttribute("type", "password")
+        passwordIcon.classList.add("hide")
+
+    }
 
 })
